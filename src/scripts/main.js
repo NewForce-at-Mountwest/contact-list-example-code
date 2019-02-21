39b7b73d15633a60d5844539a053717c00de6a4a
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-02-21 09:53:33 -0500

    deleted login files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,7 @@
 // // In main.js, import the ContactList component and the ContactForm component.
 import ContactList from "./ContactList.js"
 import ContactForm from "./ContactForm.js"
-// import LoginHandler from "./Login.js"
 
-// Uncomment this out to show the login form right when the page loads
-// document.querySelector(".output").innerHTML = LoginHandler.buildLoginForm();
 
 // Builds Contact Form
 document.querySelector(".output").innerHTML = ContactForm.buildForm();
